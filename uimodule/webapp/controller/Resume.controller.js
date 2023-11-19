29f10c947f28e4ce98f66ed6196f2e352bb69be1
Arf Mahamud <fmarif2009@gmail.com>
2023-11-19 10:19:29 +0700

    resume

--- uimodule/webapp/controller/Resume.controller.js
+++ uimodule/webapp/controller/Resume.controller.js
@@ -1,14 +1,21 @@
-sap.ui.define(["nav/test/controller/BaseController"], function (Controller) {
+sap.ui.define([
+    "nav/test/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+
+], function (Controller, JSONModel) {
     "use strict";
+
+    var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 
     return Controller.extend("nav.test.controller.Resume", {
 
         onInit: function () {
 			var oRouter = this.getRouter();
 			oRouter.getRoute("Resume").attachMatched(this._onRouteMatched, this);
+            this.getView().setModel(new JSONModel(), "view");
 		},
 		_onRouteMatched : function (oEvent) {
-			var oArgs, oView;
+			var oArgs, oView, oQuery;
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
 
@@ -24,13 +31,36 @@ sap.ui.define(["nav/test/controller/BaseController"], function (Controller) {
 					}
 				}
 			});
+            oQuery = oArgs["?query"];
+			if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1){
+				oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+			} else {
+				// the default query param should be visible at all time
+				this.getRouter().navTo("employeeResume", {
+					employeeId: oArgs.employeeId,
+					"?query": {
+						tab: _aValidTabKeys[0]
+					}
+				}, true /*no history*/);
+			}
 		},
 		_onBindingChange : function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
-		}
+		},
+        onTabSelect: function(oEvent){
+            var oCtx = this.getView().getBindingContext();
+			this.getRouter().navTo("employeeResume", {
+				employeeId: oCtx.getProperty("EmployeeID"),
+				"?query": {
+					tab: oEvent.getParameter("selectedKey")
+				}
+			}, true /*without history*/);
+
+
+        }
 
     });
 });
